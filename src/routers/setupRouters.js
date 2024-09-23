4d4120f3c10eab57e7a448f2db73e2542772b0a1
Samanta Karmaker <samonto0726@gmail.com>
2024-09-23 19:53:11 +0600

    Refactor: Add user router to setupRouters.js with register endpoint

--- src/routers/setupRouters.js
+++ src/routers/setupRouters.js
@@ -1,8 +1,10 @@
 import testRouter from "./test.router.js"
+import userRouter from "./user.router.js"
 import { API_URL } from "../constants.js"
 
 const setupRouters = app => {
 	app.use(`${API_URL}/test`, testRouter)
+	app.use(`${API_URL}/users`, userRouter)
 }
 
 export default setupRouters
---
+++ src/routers/user.router.js
@@ -0,0 +1,28 @@
+import { Router } from "express"
+import { upload } from "../middlewares/common/multer.middlewares.js"
+import { registerUser } from "../controllers/user.controllers.js"
+import {
+	registerValidator,
+	registerValidatorMiddleware,
+} from "../middlewares/user/registerValidator.middleware.js"
+
+const userRouter = Router()
+
+userRouter.post(
+	"/register",
+	upload.fields([
+		{
+			name: "avatar",
+			maxCount: 1,
+		},
+		{
+			name: "coverImage",
+			maxCount: 1,
+		},
+	]),
+	registerValidator,
+	registerValidatorMiddleware,
+	registerUser
+)
+
+export default userRouter
