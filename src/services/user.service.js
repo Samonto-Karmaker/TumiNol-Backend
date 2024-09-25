1b7cd5cf0ad27d98ae3c20268373696d76fa6e5e
Samanta Karmaker <samonto0726@gmail.com>
2024-09-25 22:50:49 +0600

    Added helper function to generate access and refresh tokens

--- src/services/user.service.js
+++ src/services/user.service.js
@@ -5,6 +5,28 @@ import {
 	uploadOnCloudinary,
 } from "../utils/cloudinary.js"
 
+// Helper functions
+const generateAccessAndRefreshToken = async userId => {
+	try {
+		const user = await User.findById(userId)
+		if (!user) {
+			throw new ApiError(404, "User not found")
+		}
+
+		const accessToken = user.generateAccessToken()
+		const refreshToken = user.generateRefreshToken()
+
+		user.refreshToken = refreshToken
+		await user.save()
+
+		return { accessToken, refreshToken }
+	} catch (error) {
+		console.error("Failed to generate access and refresh token", error)
+		throw new ApiError(500, "Failed to generate access and refresh token")
+	}
+}
+
+// Service functions
 const register = async (fullName, email, userName, password, files) => {
 	const avatarLocalPath = files?.avatar?.[0]?.path
 	if (!avatarLocalPath) {
