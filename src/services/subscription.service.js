d4bb2c9627ae3ed70ff2f393f5be8877c1714ba6
Samanta Karmaker <samonto0726@gmail.com>
2024-10-23 21:52:44 +0600

    Added toggleSubscription functionality to subscription service

---
+++ src/services/subscription.service.js
@@ -0,0 +1,50 @@
+import { Subscription } from "../models/Subscription.js"
+import ApiError from "../utils/ApiError.js"
+import { User } from "../models/User.js"
+
+// Service functions
+const toggleSubscription = async (channelId, userId) => {
+    if (!channelId) {
+        throw new ApiError(400, "Channel ID is required")
+    }
+    if (!userId) {
+        throw new ApiError(400, "User ID is required")
+    }
+    if (channelId === userId) {
+        throw new ApiError(400, "You cannot subscribe to yourself")
+    }
+
+    try {
+        let isSubscribed
+
+        const channel = await User.findById(channelId).select("_id")
+        if (!channel) {
+            throw new ApiError(404, "Channel not found")
+        }
+
+        const existingSubscriptions = await Subscription.findOne({
+            subscriber: userId,
+            channel: channelId,
+        })
+        if (!existingSubscriptions) {
+            await Subscription.create({
+                subscriber: userId,
+                channel: channelId,
+            })
+            isSubscribed = true
+        }
+        else {
+            await Subscription.findByIdAndDelete(existingSubscriptions._id)
+            isSubscribed = false
+        }
+
+        return isSubscribed
+    } catch (error) {
+        console.error("Failed to toggle subscription", error)
+        throw new ApiError(500, "Failed to toggle subscription")
+    }
+}
+
+export {
+    toggleSubscription
+}
