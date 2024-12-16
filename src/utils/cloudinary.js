a53fa25fc5681f7ca8ae9cd4fce4d63797bb9120
Samanta Karmaker <samonto0726@gmail.com>
2024-12-16 12:11:47 +0600

    Enhance cloudinary upload to support video uploads with ABR & HLS

--- src/utils/cloudinary.js
+++ src/utils/cloudinary.js
@@ -1,8 +1,6 @@
 import { v2 as cloudinary } from "cloudinary"
 import fs from "fs"
-import path from "path"
 import dotenv from "dotenv"
-import { TEMP_DIR } from "../constants.js"
 
 dotenv.config()
 
@@ -13,13 +11,30 @@ cloudinary.config({
 	api_secret: process.env.CLOUDINARY_API_SECRET,
 })
 
-const uploadOnCloudinary = async localFilePath => {
+const uploadOnCloudinary = async (localFilePath, isVideo = false) => {
 	try {
 		if (!localFilePath) return null
 		console.log(`Uploading file: ${localFilePath}`)
-		const response = await cloudinary.uploader.upload(localFilePath, {
+		const options = {
 			resource_type: "auto",
-		})
+		}
+
+		/* 	If the file is a video, we will add the eager option to generate a m3u8 file
+			Eager transformation is used to generate and store transformed versions of the image or video during the upload process
+			This allows you to perform complex manipulations on the uploaded media before delivery
+			Here, we are generating different streaming profiles for the video for Adaptive Bitrate Streaming (ABR)
+		 	format: "m3u8" is used to generate a m3u8 file which is used for streaming videos in HTTP Live Streaming (HLS) format
+		*/
+		if (isVideo) {
+			options.resource_type = "video"
+			options.eager = [
+				{ streaming_profile: "sd", format: "m3u8" }, // sd: standard definition for low bandwidth
+				{ streaming_profile: "hd", format: "m3u8" }, // hd: high definition for medium bandwidth
+				{ streaming_profile: "full_hd", format: "m3u8" }, // full_hd: full high definition for high bandwidth
+			]
+		}
+
+		const response = await cloudinary.uploader.upload(localFilePath, options)
 		console.log(`File uploaded successfully: ${response.url}`)
 		return response
 	} catch (error) {
