5241d75277e490549298902161a4168c6155b0a0
Samanta Karmaker <samonto0726@gmail.com>
2024-12-17 13:34:20 +0600

    Added video router with publish endpoint and middleware integration

---
+++ src/routers/video.router.js
@@ -0,0 +1,30 @@
+import { Router } from "express"
+import { upload } from "../middlewares/common/multer.middleware.js"
+import { checkAuth } from "../middlewares/common/authCheck.middleware.js"
+import {
+	videoDataValidator,
+	videoDataValidatorMiddleware,
+} from "../middlewares/video/videoDataValidator.middleware.js"
+import { publishVideoController } from "../controllers/video.controllers.js"
+
+const videoRouter = Router()
+
+videoRouter.post(
+	"/publish",
+	checkAuth,
+	upload.fields([
+		{
+			name: "video",
+			maxCount: 1,
+		},
+		{
+			name: "thumbnail",
+			maxCount: 1,
+		},
+	]),
+	videoDataValidator,
+	videoDataValidatorMiddleware,
+	publishVideoController
+)
+
+export default videoRouter
