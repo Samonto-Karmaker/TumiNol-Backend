4b12720cbee0f1ebb4eed8883e37138d168d98ab
Samanta Karmaker <samonto0726@gmail.com>
2024-12-18 21:31:52 +0600

    Add video router to setupRouters for video endpoint integration

--- src/routers/setupRouters.js
+++ src/routers/setupRouters.js
@@ -1,14 +1,16 @@
+import { API_URL } from "../constants.js"
 import testRouter from "./test.router.js"
 import userRouter from "./user.router.js"
-import { API_URL } from "../constants.js"
 import subscriptionRouter from "./subscription.router.js"
 import postRouter from "./post.router.js"
+import videoRouter from "./video.router.js"
 
 const setupRouters = app => {
 	app.use(`${API_URL}/test`, testRouter)
 	app.use(`${API_URL}/users`, userRouter)
 	app.use(`${API_URL}/subscriptions`, subscriptionRouter)
 	app.use(`${API_URL}/posts`, postRouter)
+	app.use(`${API_URL}/videos`, videoRouter)
 }
 
 export default setupRouters
