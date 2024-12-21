ecfd6e161104b2cec414adc774c9a080b664de13
Samanta Karmaker <samonto0726@gmail.com>
2024-12-21 14:09:13 +0600

    Added video sorting options and orders enums to constants

--- src/constants.js
+++ src/constants.js
@@ -8,4 +8,16 @@ const TEMP_DIR = path.join(__dirname, "..", "public", "temp")
 const DB_NAME = "tuminol"
 const API_URL = "/api/v1"
 
-export { DB_NAME, API_URL, TEMP_DIR }
+const VideoSortOptionsEnums = Object.freeze({
+    CREATED_AT: "createdAt",
+    VIEWS: "views",
+    LIKES: "likes",
+    DURATION: "duration",
+})
+
+const VideoSortOrdersEnums = Object.freeze({
+    ASC: "asc",
+    DESC: "desc",
+})
+
+export { DB_NAME, API_URL, TEMP_DIR, VideoSortOptionsEnums, VideoSortOrdersEnums }
