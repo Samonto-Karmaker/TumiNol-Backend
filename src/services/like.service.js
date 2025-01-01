a6051e9a0a85c6f0071f74eb5ce6ee308b9c3d1b
Samanta Karmaker <samonto0726@gmail.com>
2025-01-01 22:02:17 +0600

    Initial like service setup

---
+++ src/services/like.service.js
@@ -0,0 +1,9 @@
+const toggleVideoLike = async (videoId, userId) => {}
+
+const toggleCommentLike = async (commentId, userId) => {}
+
+const togglePostLike = async (postId, userId) => {}
+
+const getLikedVideos = async userId => {}
+
+export { toggleVideoLike, toggleCommentLike, togglePostLike, getLikedVideos }
