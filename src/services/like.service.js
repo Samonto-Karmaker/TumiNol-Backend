f57fbabfe8a9ea7ad5c9ac8f21e9c07d0f0970f0
Samanta Karmaker <samonto0726@gmail.com>
2025-01-08 22:52:50 +0600

    feat: implemented toggle video like service

--- src/services/like.service.js
+++ src/services/like.service.js
@@ -1,4 +1,28 @@
-const toggleVideoLike = async (videoId, userId) => {}
+import { Like } from "../models/Like.js"
+import ApiError from "../utils/ApiError.js"
+import { isValidObjectId } from "../utils/validateObjectId.js"
+
+const toggleVideoLike = async (videoId, userId) => {
+    if (!videoId || !isValidObjectId(videoId)) {
+        throw new ApiError(400, "Invalid videoId")
+    }
+    if (!userId) {
+        throw new ApiError(400, "Invalid userId")
+    }
+    try {
+        const like = await Like.findOne({ video: videoId, likedBy: userId })
+        if (like) {
+            await like.remove()
+            return { message: "unlike video" }
+        } else {
+            await Like.create({ video: videoId, likedBy: userId })
+            return { message: "like video" }
+        }
+    } catch (error) {
+        console.error("Failed to toggle video like: ", error)
+        throw new ApiError(500, "Internal Server Error")
+    }
+}
 
 const toggleCommentLike = async (commentId, userId) => {}
 
