6a9bfc6e2c83a4c36ab5ef43a58689c468dd8be2
Samanta Karmaker <samonto0726@gmail.com>
2025-01-13 22:04:26 +0600

    feat: added toggle comment like endpoint

--- src/routers/like.router.js
+++ src/routers/like.router.js
@@ -1,6 +1,7 @@
 import { Router } from "express"
 import { checkAuth } from "../middlewares/common/authCheck.middleware.js"
 import {
+	toggleCommentLikeController,
 	togglePostLikeController,
 	toggleVideoLikeController,
 } from "../controllers/like.controllers.js"
@@ -10,5 +11,6 @@ likeRouter.use(checkAuth)
 
 likeRouter.post("/video/:videoId", toggleVideoLikeController)
 likeRouter.post("/post/:postId", togglePostLikeController)
+likeRouter.post("/comment/:commentId", toggleCommentLikeController)
 
 export default likeRouter
