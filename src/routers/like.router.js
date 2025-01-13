b5e3117b2380a030c4ca6488a264a34d03154384
Samanta Karmaker <samonto0726@gmail.com>
2025-01-13 22:00:08 +0600

    added toggle post like endpoint

--- src/routers/like.router.js
+++ src/routers/like.router.js
@@ -1,10 +1,14 @@
 import { Router } from "express"
 import { checkAuth } from "../middlewares/common/authCheck.middleware.js"
-import { toggleVideoLikeController } from "../controllers/like.controllers.js"
+import {
+	togglePostLikeController,
+	toggleVideoLikeController,
+} from "../controllers/like.controllers.js"
 
 const likeRouter = Router()
 likeRouter.use(checkAuth)
 
 likeRouter.post("/video/:videoId", toggleVideoLikeController)
+likeRouter.post("/post/:postId", togglePostLikeController)
 
 export default likeRouter
