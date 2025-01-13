720f3c9f90c7d1aefc2afd7d7cf349f6d398cfe3
Samanta Karmaker <samonto0726@gmail.com>
2025-01-13 22:26:49 +0600

    fix: can't like videos that are not published

--- src/services/like.service.js
+++ src/services/like.service.js
@@ -6,95 +6,92 @@ import { isValidObjectId } from "../utils/validateObjectId.js"
 
 // Helper functions
 const toggleLike = async (resourceObject, userId) => {
-    try {
-        const like = await Like.findOne({ ...resourceObject, likedBy: userId })
-        if (like) {
-            await Like.findByIdAndDelete(like._id)
-            return { message: "unlike" }
-        } else {
-            await Like.create({ ...resourceObject, likedBy: userId })
-            return { message: "like" }
-        }
-    } catch (error) {
-        console.error("Failed to toggle like: ", error)
-        throw new ApiError(500, "Internal Server Error")
-    }
+	try {
+		const like = await Like.findOne({ ...resourceObject, likedBy: userId })
+		if (like) {
+			await Like.findByIdAndDelete(like._id)
+			return { message: "unlike" }
+		} else {
+			await Like.create({ ...resourceObject, likedBy: userId })
+			return { message: "like" }
+		}
+	} catch (error) {
+		console.error("Failed to toggle like: ", error)
+		throw new ApiError(500, "Internal Server Error")
+	}
 }
 
 // Service functions
 const toggleVideoLike = async (videoId, userId) => {
-    if (!videoId || !isValidObjectId(videoId)) {
-        throw new ApiError(400, "Invalid videoId")
-    }
-    if (!userId) {
-        throw new ApiError(400, "Invalid userId")
-    }
+	if (!videoId || !isValidObjectId(videoId)) {
+		throw new ApiError(400, "Invalid videoId")
+	}
+	if (!userId) {
+		throw new ApiError(400, "Invalid userId")
+	}
 
-    try {
-        const video = await Video.findById(videoId).select("_id")
-        if (!video) {
-            throw new ApiError(404, "Video not found")
-        }
-    }
-    catch (error) {
-        if (error instanceof ApiError) {
-            throw error
-        }
-        console.error("Failed to find video: ", error)
-        throw new ApiError(500, "Internal Server Error")
-    }
+	try {
+		const video = await Video.findById(videoId).select("_id isPublished")
+		if (!video || !video.isPublished) {
+			throw new ApiError(404, "Video not found")
+		}
+	} catch (error) {
+		if (error instanceof ApiError) {
+			throw error
+		}
+		console.error("Failed to find video: ", error)
+		throw new ApiError(500, "Internal Server Error")
+	}
 
-    return toggleLike({ video: videoId }, userId)
+	return toggleLike({ video: videoId }, userId)
 }
 
 const toggleCommentLike = async (commentId, userId) => {
-    if (!commentId || !isValidObjectId(commentId)) {
-        throw new ApiError(400, "Invalid commentId")
-    }
-    if (!userId) {
-        throw new ApiError(400, "Invalid userId")
-    }
+	if (!commentId || !isValidObjectId(commentId)) {
+		throw new ApiError(400, "Invalid commentId")
+	}
+	if (!userId) {
+		throw new ApiError(400, "Invalid userId")
+	}
 
-    try {
-        const comment = await Comment.findById(commentId).select("_id")
-        if (!comment) {
-            throw new ApiError(404, "Comment not found")
-        }
-    }
-    catch (error) {
-        if (error instanceof ApiError) {
-            throw error
-        }
-        console.error("Failed to find comment: ", error)
-        throw new ApiError(500, "Internal Server Error")
-    }
+	try {
+		const comment = await Comment.findById(commentId).select("_id")
+		if (!comment) {
+			throw new ApiError(404, "Comment not found")
+		}
+	} catch (error) {
+		if (error instanceof ApiError) {
+			throw error
+		}
+		console.error("Failed to find comment: ", error)
+		throw new ApiError(500, "Internal Server Error")
+	}
 
-    return toggleLike({ comment: commentId }, userId)
+	return toggleLike({ comment: commentId }, userId)
 }
 
 const togglePostLike = async (postId, userId) => {
-    if (!postId || !isValidObjectId(postId)) {
-        throw new ApiError(400, "Invalid postId")
-    }
-    if (!userId) {
-        throw new ApiError(400, "Invalid userId")
-    }
+	if (!postId || !isValidObjectId(postId)) {
+		throw new ApiError(400, "Invalid postId")
+	}
+	if (!userId) {
+		throw new ApiError(400, "Invalid userId")
+	}
 
-    try {
-        const post = await Post.findById(postId).select("_id")
-        if (!post) {
-            throw new ApiError(404, "Post not found")
-        }
-    }
-    catch (error) {
-        if (error instanceof ApiError) {
-            throw error
-        }
-        console.error("Failed to find post: ", error)
-        throw new ApiError(500, "Internal Server Error")
-    }
+	try {
+		const post = await Post.findById(postId).select("_id")
+		if (!post) {
+			throw new ApiError(404, "Post not found")
+		}
+	} catch (error) {
+		if (error instanceof ApiError) {
+			throw error
+		}
+		console.error("Failed to find post: ", error)
+		throw new ApiError(500, "Internal Server Error")
+	}
 
-    return toggleLike({ post: postId }, userId)
+	return toggleLike({ post: postId }, userId)
 }
 
 const getLikedVideos = async userId => {}
