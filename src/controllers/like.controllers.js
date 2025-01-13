9808b0f33d620c852e7e34d3f41db31e8f1a19a3
Samanta Karmaker <samonto0726@gmail.com>
2025-01-13 22:03:35 +0600

    feat: added toggle comment like controller

--- src/controllers/like.controllers.js
+++ src/controllers/like.controllers.js
@@ -3,21 +3,28 @@ import ApiResponse from "../utils/ApiResponse.js"
 import { togglePostLike, toggleVideoLike } from "../services/like.service.js"
 
 const toggleVideoLikeController = asyncHandler(async (req, res) => {
-    const videoId = req.params.videoId
-    const userId = req.user.id
-    const response = await toggleVideoLike(videoId, userId)
-    res
-        .status(200)
-        .json(new ApiResponse(200, response.message))
+	const videoId = req.params.videoId
+	const userId = req.user.id
+	const response = await toggleVideoLike(videoId, userId)
+	res.status(200).json(new ApiResponse(200, response.message))
 })
 
 const togglePostLikeController = asyncHandler(async (req, res) => {
-    const postId = req.params.postId
-    const userId = req.user.id
-    const response = await togglePostLike(postId, userId)
-    res
-        .status(200)
-        .json(new ApiResponse(200, response.message))
+	const postId = req.params.postId
+	const userId = req.user.id
+	const response = await togglePostLike(postId, userId)
+	res.status(200).json(new ApiResponse(200, response.message))
 })
 
-export { toggleVideoLikeController, togglePostLikeController }
+const toggleCommentLikeController = asyncHandler(async (req, res) => {
+	const commentId = req.params.commentId
+	const userId = req.user.id
+	const response = await toggleCommentLike(commentId, userId)
+	res.status(200).json(new ApiResponse(200, response.message))
+})
+
+export {
+	toggleVideoLikeController,
+	togglePostLikeController,
+	toggleCommentLikeController,
+}
