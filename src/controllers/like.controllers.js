62d9d9dc85f7ad6d85c236c013fcdc89751022a1
Samanta Karmaker <samonto0726@gmail.com>
2025-01-14 21:04:31 +0600

    feat: added getLikedVideosController to retrieve user's liked videos with pagination

--- src/controllers/like.controllers.js
+++ src/controllers/like.controllers.js
@@ -1,30 +1,41 @@
 import asyncHandler from "../utils/asyncHandler.js"
 import ApiResponse from "../utils/ApiResponse.js"
-import { togglePostLike, toggleVideoLike } from "../services/like.service.js"
+import { getLikedVideos, togglePostLike, toggleVideoLike } from "../services/like.service.js"
+import { STANDARD_LIMIT_PER_PAGE } from "../constants.js"
 
 const toggleVideoLikeController = asyncHandler(async (req, res) => {
 	const videoId = req.params.videoId
-	const userId = req.user.id
+	const userId = req.user._id
 	const response = await toggleVideoLike(videoId, userId)
 	res.status(200).json(new ApiResponse(200, response.message))
 })
 
 const togglePostLikeController = asyncHandler(async (req, res) => {
 	const postId = req.params.postId
-	const userId = req.user.id
+	const userId = req.user._id
 	const response = await togglePostLike(postId, userId)
 	res.status(200).json(new ApiResponse(200, response.message))
 })
 
 const toggleCommentLikeController = asyncHandler(async (req, res) => {
 	const commentId = req.params.commentId
-	const userId = req.user.id
+	const userId = req.user._id
 	const response = await toggleCommentLike(commentId, userId)
 	res.status(200).json(new ApiResponse(200, response.message))
+})
+
+const getLikedVideosController = asyncHandler(async (req, res) => {
+	const userId = req.user._id
+	let { page, limit } = req.query
+	page = parseInt(page) || 1
+	limit = parseInt(limit) || STANDARD_LIMIT_PER_PAGE
+	const likedVideos = await getLikedVideos(userId, page, limit)
+	res.status(200).json(new ApiResponse(200, likedVideos))
 })
 
 export {
 	toggleVideoLikeController,
 	togglePostLikeController,
 	toggleCommentLikeController,
+	getLikedVideosController,
 }
