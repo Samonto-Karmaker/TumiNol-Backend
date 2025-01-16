7601835cb68125c72d94630767431af84238ba07
Samanta Karmaker <samonto0726@gmail.com>
2025-01-16 21:55:32 +0600

    feat: add comment controller for adding comments to videos

---
+++ src/controllers/comment.controllers.js
@@ -0,0 +1,13 @@
+import asyncHandler from "../utils/asyncHandler.js"
+import ApiResponse from "../utils/ApiResponse.js"
+import { addComment } from "../services/comment.service"
+
+const addCommentController = asyncHandler(async (req, res) => {
+	const videoId = req.params.videoId
+	const userId = req.user._id
+	const comment = req.body.comment
+	const newComment = await addComment(userId, videoId, comment)
+	res.status(201).json(new ApiResponse(201, "Comment added", newComment))
+})
+
+export { addCommentController }
