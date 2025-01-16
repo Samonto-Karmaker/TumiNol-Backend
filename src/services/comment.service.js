4d68b84a887669a95acc9a05f748b785f86b0889
Samanta Karmaker <samonto0726@gmail.com>
2025-01-16 21:18:46 +0600

    feat: initial comment service setup

---
+++ src/services/comment.service.js
@@ -0,0 +1,9 @@
+const getVideoComments = async videoId => {}
+
+const addComment = async (videoId, comment) => {}
+
+const updateComment = async (commentId, comment) => {}
+
+const deleteComment = async commentId => {}
+
+export { getVideoComments, addComment, updateComment, deleteComment }
