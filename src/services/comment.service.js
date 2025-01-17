ee966eea4c0fc428ebc51bd60aea129c045baf1e
Samanta Karmaker <samonto0726@gmail.com>
2025-01-17 22:00:49 +0600

    feat: implement get video comment with pagination

--- src/services/comment.service.js
+++ src/services/comment.service.js
@@ -2,8 +2,104 @@ import { isValidObjectId } from "../utils/validateObjectId.js"
 import ApiError from "../utils/ApiError.js"
 import { Video } from "../models/Video.js"
 import { Comment } from "../models/Comment.js"
+import {
+	STANDARD_LIMIT_PER_PAGE,
+	HIGHEST_LIMIT_PER_PAGE,
+} from "../constants.js"
+import PaginationResponseDTO from "../DTOs/PaginationResponseDTO.js"
 
-const getVideoComments = async videoId => {}
+const getVideoComments = async (
+	videoId,
+	accessingUserId,
+	page = 1,
+	limit = STANDARD_LIMIT_PER_PAGE
+) => {
+	if (!videoId || !isValidObjectId(videoId)) {
+		throw new ApiError(400, "Invalid video ID")
+	}
+	if (!accessingUserId) {
+		throw new ApiError(400, "User ID is required")
+	}
+	if (page < 1 || limit < 1 || limit > HIGHEST_LIMIT_PER_PAGE) {
+		throw new ApiError(400, "Invalid page or limit")
+	}
+	try {
+		const video = await Video.findById(videoId).select("_id isPublished")
+		if (!video || !video.isPublished) {
+			throw new ApiError(404, "Video not found")
+		}
+		const totalComments = await Comment.countDocuments({ video: videoId })
+		const totalPages = Math.ceil(totalComments / limit)
+		if (page > totalPages) {
+			throw new ApiError(404, "No comments found")
+		}
+		if (totalComments === 0) {
+			return new PaginationResponseDTO([], totalComments, totalPages, 0)
+		}
+
+		const comments = await Comment.aggregate([
+			{
+				$match: { video: videoId },
+			},
+			{
+				$lookup: {
+					from: "users",
+					localField: "owner",
+					foreignField: "_id",
+					as: "owner",
+				},
+			},
+			{
+				$unwind: "$owner",
+			},
+			{
+				$lookup: {
+					from: "likes",
+					localField: "_id",
+					foreignField: "comment",
+					as: "likes",
+				},
+			},
+			{
+				$addFields: {
+					likeCount: { $size: "$likes" },
+					isLiked: {
+						$in: [accessingUserId, "$likes.owner"],
+					},
+				},
+			},
+			{
+				$project: {
+					content: 1,
+					createdAt: 1,
+					likeCount: 1,
+					isLiked: 1,
+					owner: {
+						_id: 1,
+						fullName: 1,
+						avatar: 1,
+					},
+				},
+			},
+			{
+				$sort: { createAt: -1 },
+			},
+			{
+				$skip: (page - 1) * limit,
+			},
+			{
+				$limit: limit,
+			},
+		])
+
+		return new PaginationResponseDTO(comments, totalComments, totalPages, page)
+	} catch (error) {
+		if (error instanceof ApiError) {
+			throw error
+		}
+		throw new ApiError(500, error.message)
+	}
+}
 
 const addComment = async (userId, videoId, comment) => {
 	if (!videoId || !isValidObjectId(videoId)) {
