34776b027d2f2689469cae3fba01bf89df5babda
Samanta Karmaker <samonto0726@gmail.com>
2025-01-17 21:16:57 +0600

    feat: add update comment endpoint

--- src/routers/comment.router.js
+++ src/routers/comment.router.js
@@ -1,11 +1,12 @@
 import { Router } from "express"
 import { checkAuth } from "../middlewares/common/authCheck.middleware.js"
-import { addCommentController } from "../controllers/comment.controllers.js"
+import { addCommentController, updateCommentController } from "../controllers/comment.controllers.js"
 
 const commentRouter = Router()
 
 commentRouter.use(checkAuth)
 
 commentRouter.route("/:videoId").post(addCommentController)
+commentRouter.route("/:commentId").patch(updateCommentController)
 
 export default commentRouter
