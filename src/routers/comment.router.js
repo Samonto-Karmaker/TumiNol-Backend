dbf82083f9e10ea3aab6646f1613fc3ef3094327
Samanta Karmaker <samonto0726@gmail.com>
2025-01-17 21:27:07 +0600

    feat: add delete comment endpoint

--- src/routers/comment.router.js
+++ src/routers/comment.router.js
@@ -1,12 +1,19 @@
 import { Router } from "express"
 import { checkAuth } from "../middlewares/common/authCheck.middleware.js"
-import { addCommentController, updateCommentController } from "../controllers/comment.controllers.js"
+import {
+	addCommentController,
+	deleteCommentController,
+	updateCommentController,
+} from "../controllers/comment.controllers.js"
 
 const commentRouter = Router()
 
 commentRouter.use(checkAuth)
 
 commentRouter.route("/:videoId").post(addCommentController)
-commentRouter.route("/:commentId").patch(updateCommentController)
+commentRouter
+	.route("/:commentId")
+	.patch(updateCommentController)
+	.delete(deleteCommentController)
 
 export default commentRouter
