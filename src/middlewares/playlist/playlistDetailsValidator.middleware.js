5278dc8c6ccbdea895e878076bfc2e285339e918
Samanta Karmaker <samonto0726@gmail.com>
2025-01-20 23:10:58 +0600

    feat: add playlist details validation middleware

---
+++ src/middlewares/playlist/playlistDetailsValidator.middleware.js
@@ -0,0 +1,24 @@
+import { check, validationResult } from "express-validator"
+import ApiError from "../../utils/ApiError.js"
+
+const playlistDetailsValidator = [
+	check("title")
+		.isLength({ min: 3, max: 100 })
+		.withMessage("Title should be between 3 to 100 characters")
+		.trim(),
+	check("description")
+		.isLength({ min: 3, max: 1000 })
+		.withMessage("Description should be between 3 to 1000 characters")
+		.trim(),
+]
+
+const playlistDetailsValidatorMiddleware = (req, res, next) => {
+	const errors = validationResult(req)
+	const mappedErrors = errors.mapped()
+	if (Object.keys(mappedErrors).length === 0) {
+		return next()
+	}
+	res.send(400).json(new ApiError(400, "Invalid playlist details", mappedErrors))
+}
+
+export { playlistDetailsValidator, playlistDetailsValidatorMiddleware }
