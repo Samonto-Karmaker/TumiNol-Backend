accebb9faf139871a4afa4dbd947777ae6560986
Samanta Karmaker <samonto0726@gmail.com>
2025-01-21 21:37:30 +0600

    feat: update createPlaylistController to include success message in response

--- src/controllers/playlist.controllers.js
+++ src/controllers/playlist.controllers.js
@@ -1,10 +1,11 @@
 import asyncHandler from "../utils/asyncHandler.js"
 import ApiResponse from "../utils/ApiResponse.js"
+import { createPlaylist } from "../services/playlist.service.js"
 
 const createPlaylistController = asyncHandler(async (req, res) => {
 	const { title, description } = req.body
 	const newPlaylist = await createPlaylist(req.user._id, { title, description })
-	res.status(201).json(new ApiResponse(201, newPlaylist))
+	res.status(201).json(new ApiResponse(201, "Playlist created", newPlaylist))
 })
 
 export { createPlaylistController }
