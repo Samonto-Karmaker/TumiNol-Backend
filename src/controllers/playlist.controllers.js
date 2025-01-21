6e34eed97657d327e4940b2d9bc406b8180bd86b
Samanta Karmaker <samonto0726@gmail.com>
2025-01-21 22:31:33 +0600

    feat: add getPlaylistsByOwner controller to fetch playlists by ownerId

--- src/controllers/playlist.controllers.js
+++ src/controllers/playlist.controllers.js
@@ -1,6 +1,10 @@
 import asyncHandler from "../utils/asyncHandler.js"
 import ApiResponse from "../utils/ApiResponse.js"
-import { createPlaylist } from "../services/playlist.service.js"
+import {
+	createPlaylist,
+	getPlaylistsByOwner,
+} from "../services/playlist.service.js"
+import { STANDARD_LIMIT_PER_PAGE } from "../constants.js"
 
 const createPlaylistController = asyncHandler(async (req, res) => {
 	const { title, description } = req.body
@@ -8,4 +12,13 @@ const createPlaylistController = asyncHandler(async (req, res) => {
 	res.status(201).json(new ApiResponse(201, "Playlist created", newPlaylist))
 })
 
-export { createPlaylistController }
+const getPlaylistsByOwnerController = asyncHandler(async (req, res) => {
+	let { page, limit } = req.query
+	page = parseInt(page) || 1
+	limit = parseInt(limit) || STANDARD_LIMIT_PER_PAGE
+	const ownerId = req.params.ownerId
+	const playlists = await getPlaylistsByOwner(ownerId, page, limit)
+	res.status(200).json(new ApiResponse(200, "Playlists fetched", playlists))
+})
+
+export { createPlaylistController, getPlaylistsByOwnerController }
