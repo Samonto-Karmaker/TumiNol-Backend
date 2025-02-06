4d37aadcc4304261b3f710a77088c0660062a71e
Samanta Karmaker <samonto0726@gmail.com>
2025-02-06 23:33:45 +0600

    feat: add get top performing videos endpoint

--- src/routers/analytics.router.js
+++ src/routers/analytics.router.js
@@ -1,8 +1,13 @@
 import { Router } from "express"
-import { getChannelStatsController } from "../controllers/analytics.controllers.js"
+import {
+	getChannelStatsController,
+	getTopPerformingVideosController,
+} from "../controllers/analytics.controllers.js"
+import { checkAuth } from "../middlewares/common/authCheck.middleware.js"
 
 const analyticsRouter = Router()
 
 analyticsRouter.get("/channel-stats/:userId", getChannelStatsController)
+analyticsRouter.get("/top-videos", checkAuth, getTopPerformingVideosController)
 
 export default analyticsRouter
