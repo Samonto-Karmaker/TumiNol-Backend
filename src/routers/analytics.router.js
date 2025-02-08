70ed96fa6ef0fe345070c12a09539d0a4963ac55
Samanta Karmaker <samonto0726@gmail.com>
2025-02-08 22:03:12 +0600

    feat: add get top posts endpoint

--- src/routers/analytics.router.js
+++ src/routers/analytics.router.js
@@ -2,6 +2,7 @@ import { Router } from "express"
 import {
 	getChannelStatsController,
 	getTopPerformingVideosController,
+	getTopPostsController,
 } from "../controllers/analytics.controllers.js"
 import { checkAuth } from "../middlewares/common/authCheck.middleware.js"
 
@@ -9,5 +10,6 @@ const analyticsRouter = Router()
 
 analyticsRouter.get("/channel-stats/:userId", getChannelStatsController)
 analyticsRouter.get("/top-videos", checkAuth, getTopPerformingVideosController)
+analyticsRouter.get("/top-posts", checkAuth, getTopPostsController)
 
 export default analyticsRouter
